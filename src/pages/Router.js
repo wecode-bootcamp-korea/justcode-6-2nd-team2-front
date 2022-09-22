4916fdedeb815ef53802dcf94edd2ff87268049c
Miog Yang <99234582+miogy@users.noreply.github.com>
2022-09-22 20:31:13 +0900

    Add : Header
    * ADD : subNav
    
    * ADD : nav완료
    
    * MOD: 카테링크수정
    
    * ADD : signup
    
    * ADD : signup-tab
    
    * ADD : signup

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -8,12 +8,17 @@ import GlobalStyle from '../styles/GlobalStyle';
 import Main from './Main/Main';
 import Movie from './Movie/Movie';
 import MovieContent from '../components/Movie/MovieContent';
+import Header from '../components/Header/Header';
+import UserFind from './Find/UserFind';
+import PassFind from './Find/PassFind';
+import SignUp from './SignUp/SignUp';
 
 function Router() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/movie' element={<Movie />}>
@@ -21,6 +26,10 @@ function Router() {
             <Route path='b' element={<MovieContent />} />
             <Route path='c' element={<MovieContent />} />
           </Route>
+
+          <Route path='/user-find' element={<UserFind />} />
+          <Route path='/pass-find' element={<PassFind />} />
+          <Route path='/signup' element={<SignUp />} />
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
---
+++ src/pages/SignUp/Auth.js
@@ -0,0 +1,36 @@
+import styles from './Auth.module.scss';
+import { GiSmartphone } from 'react-icons/gi';
+import { SiNaver } from 'react-icons/si';
+
+function Auth() {
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.content}>
+        <h1 className={styles.title}>회원가입을 위함 본인인증 단계입니다.</h1>
+        <p className={styles.titleText}>원하시는 인증방법을 선택해주세요.</p>
+        <div className={styles.boxContent}>
+          <button>
+            <GiSmartphone size='80px' color='lightgray' />
+            <p>휴대폰 인증</p>
+          </button>
+          <button className={styles.naver}>
+            <SiNaver size='50px' color='lightgray' />
+            <p style={{ paddingTop: '20px' }}>네이버 로그인</p>
+          </button>
+        </div>
+        <div>
+          <ul>
+            <li className={styles.titleText}>
+              - 14세 미만 어린이는 보호자 인증을 추가로 완료한 후 가입이 가능합니다.
+            </li>
+            <li className={styles.titleText}>
+              - 본인인증 시 제공되는 정보는 해당 인증기관에서 직접 수집하며, 인증 이외의 용도로 이용
+              또는 저장되지 않습니다.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Auth;
