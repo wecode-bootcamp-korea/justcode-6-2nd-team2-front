5fa4bdce50bd57a85cb796cdc2ff1ed3cca4b756
miogy <jjangrl87@gmail.com>
2022-09-29 14:35:12 +0900

    ADD : router수정

--- src/pages/Router.js
+++ src/pages/Router.js
@@ -24,44 +24,45 @@ import Info from './SignUp/Info';
 import Complete from './SignUp/Complete';
 import Footer from '../components/Footer/Footer';
 import TimeTable from './TimeTable/TimeTable';
+import ListTheater from '../components/Booking/Theater/ListTheater';
+import MovieComment from './MovieDetail/MovieComment';
 
 const Context10 = createContext({
   scheduleId: '',
   setScheduleId: () => {},
 });
-import ListTheater from '../components/Booking/Theater/ListTheater';
-import MovieComment from './MovieDetail/MovieComment';
 
 function Router() {
   const [scheduleId, setScheduleId] = useState();
 
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Main />} />
-          <Route path='/Booking/*' element={<Booking />} />
-          <Route path='/TimeTable/*' element={<TimeTable />} />
-          {/* 영화페이지 */}
-          <Route path='/movie' element={<Movie />}>
-            <Route index element={<MovieContent />} />
-            <Route path='now' element={<MovieContent />} />
-            <Route path='upcoming' element={<MovieContent />} />
-            <Route path='domestic' element={<MovieContent />} />
-            <Route path='abroad' element={<MovieContent />} />
-          </Route>
+    <Context10.Provider value={{ scheduleId, setScheduleId }}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Main />} />
+            <Route path='/Booking/*' element={<Booking />} />
+            <Route path='/TimeTable/*' element={<TimeTable />} />
+            {/* 영화페이지 */}
+            <Route path='/movie' element={<Movie />}>
+              <Route index element={<MovieContent />} />
+              <Route path='now' element={<MovieContent />} />
+              <Route path='upcoming' element={<MovieContent />} />
+              <Route path='domestic' element={<MovieContent />} />
+              <Route path='abroad' element={<MovieContent />} />
+            </Route>
 
-          {/* 영화 상세페이지 */}
-          <Route path='/moviedetail' end element={<MovieDetail />}>
-            {/* <Route index element={<MovieInfo />} /> */}
-            <Route path='movieinfo' element={<MovieInfo />} />
-            <Route path='comment' element={<MovieComment />} />
-            <Route path='moviepost' element={<MoviePost />} />
-            <Route path='trailer' element={<MovieTrailer />} />
-          </Route>
-          {/* <Route path='/theater' element={<ListTheater />} /> */}
+            {/* 영화 상세페이지 */}
+            <Route path='/moviedetail' end element={<MovieDetail />}>
+              {/* <Route index element={<MovieInfo />} /> */}
+              <Route path='movieinfo' element={<MovieInfo />} />
+              <Route path='comment' element={<MovieComment />} />
+              <Route path='moviepost' element={<MoviePost />} />
+              <Route path='trailer' element={<MovieTrailer />} />
+            </Route>
+            {/* <Route path='/theater' element={<ListTheater />} /> */}
 
             <Route path='/user-find' element={<UserFind />} />
             <Route path='/pass-find' element={<PassFind />} />
