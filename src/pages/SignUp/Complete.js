9c514da79636554171adf7e39ad4aff3ebe5cb42
miogy <jjangrl87@gmail.com>
2022-09-30 10:57:02 +0900

    ADD : merge

--- src/pages/SignUp/Complete.js
+++ src/pages/SignUp/Complete.js
@@ -10,7 +10,7 @@ function Complete() {
       <h1>스타박스 가입을 환영합니다.</h1>
       <p>이제부터 스타박스에서 제공하는 다양한 멤버십 혜택을 이용할 수 있습니다.</p>
 
-      <button onClick={() => navigate('/main')}>가입완료</button>
+      <button onClick={() => navigate('/')}>가입완료</button>
     </div>
   );
 }
