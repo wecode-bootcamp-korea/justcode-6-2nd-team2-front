9eff3a36aa3beba765b33efb8e01671fa976abb1
Eden Park <109200582+daydreamplace@users.noreply.github.com>
2022-09-30 10:02:14 +0900

    [MOD]: 메인페이지 코드, UI 수정
    * [MOD]: 메인페이지 코드 수정
    
    * [MOD]: 메인페이지 UI 수정
    
    * [MOD]: 메인페이지 UI 수정
    
    * [MOD]: 메인페이지 검색 UI 수정
    
    * [MOD]: 메인페이지 코드, UI 수정
    
    * [MOD]: 메인페이지 코드, UI 수정
    
    * [MOD]: 메인페이지 코드, UI 수정
    
    * [MOD]: 코드 수정

--- src/components/Movie/MovieList.js
+++ src/components/Movie/MovieList.js
@@ -3,25 +3,32 @@ import styled from 'styled-components';
 
 import MovieCard from './MovieCard';
 import ListViewMore from './ListViewMore';
+import DataNull from './DataNull';
 
 function MovieList({ movieList, onLoadMore, likeLoader, alertModal, setAlertModal }) {
   return (
     <>
-      <CardList>
-        {movieList.map(card => {
-          return (
-            <li key={card.id}>
-              <MovieCard
-                data={card}
-                likeLoader={likeLoader}
-                alertModal={alertModal}
-                setAlertModal={setAlertModal}
-              />
-            </li>
-          );
-        })}
-      </CardList>
-      <ListViewMore onLoadMore={onLoadMore} />
+      {Array.isArray(movieList) && movieList.length > 0 ? (
+        <>
+          <CardList>
+            {movieList.map(card => {
+              return (
+                <li key={card.id}>
+                  <MovieCard
+                    data={card}
+                    likeLoader={likeLoader}
+                    alertModal={alertModal}
+                    setAlertModal={setAlertModal}
+                  />
+                </li>
+              );
+            })}
+          </CardList>
+          <ListViewMore onLoadMore={onLoadMore} text='더보기' />
+        </>
+      ) : (
+        <DataNull />
+      )}
     </>
   );
 }
